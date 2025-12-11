da11e816683375ae0f6ccb05ec78af66cc0da0e9
Simon Hampel <simon@hampelgroup.com>
2025-12-11 16:34:40 +1100

    bump version to 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 CHANGELOG
 =========
 
+1.0.2 (2025-12-11)
+------------------
+
+* run enqueuePostUpgradeCleanUp during upgrades if we're running XF2.3+
+
 1.0.1 (2024-07-11)
 ------------------
 
---
+++ _output/option_hint.php
@@ -0,0 +1,18 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property string|null $hampelHideSidebarNodes Nodes to hide sidebar
+ */
+class Options
+{
+}
